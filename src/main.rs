fef90cc6bf3d184b9830fdec36afd9029aeaf405
NeoNine50 <89785098+NeoNine50@users.noreply.github.com>
2024-11-07 17:45:58 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 /*
-* Copyright 2018-2019 Michal Mauser
+* Copyright 2018-2019 Michal Mauser (Original source)
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as published by
@@ -17,8 +17,6 @@
 
 #[macro_use] extern crate rocket;
 
-//#[cfg(test)] mod tests;
-
 use rocket::tokio::sync::Mutex;
 use rocket_sync_db_pools::rusqlite::Connection;
 use rocket::serde::Deserialize;
@@ -30,11 +28,8 @@ use webbrowser;
 use rocket::figment;
 
 mod users;
-//use users::*;
 mod products;
-//use products::*;
 mod transfers;
-//use transfers::*;
 mod db;
 
 pub type DbConn = Mutex<Connection>;
@@ -48,29 +43,39 @@ fn index(flash: Option<FlashMessage>) -> Template {
         Some(x) => Template::render("index", x.message()),
         _ => Template::render("index", "")
     }
-    /*let mut context = HashMap::new();
-    if let Some(ref msg) = flash {
-        context.insert("flash", msg.msg());
-    }
-
-    Template::render("login", &context)*/
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let conn = Connection::open("diet_tracker.sqlite").expect("db file");
 
-    // Initialize the `entries` table in the database.
+    // Initialize the entries table in the database.
     db::init_database(&conn);
 
-    //let rct = rocket::ignite()
+    // Build the Rocket application and attach routes and template engine
     let mut rct = rocket::build()
         .attach(Template::fairing())
         .manage(Mutex::new(conn))
-        .mount("/", routes![index, users::adduser_page, products::addproduct_page, products::addproduct, products::product_page, users::adduser,
-        transfers::transfer_page, transfers::transfer, transfers::transfers, users::users, products::products, transfers::delete_transfer,
-        users::addproduct, users::product_page, products::product_producers, users::fame]);
+        .mount("/", routes![
+            index,
+            users::adduser_page,
+            products::addproduct_page,
+            products::addproduct,
+            products::product_page,
+            users::adduser,
+            transfers::transfer_page,
+            transfers::transfer,
+            transfers::transfers,
+            users::users,
+            products::products,
+            transfers::delete_transfer,
+            users::addproduct,
+            users::product_page,
+            products::product_producers,
+            users::fame,
+        ]);
 
+    // Automatically open the browser if the "gui" feature is enabled
     #[cfg(feature = "gui")] {
         rct = rct.attach(AdHoc::on_liftoff("Liftoff Printer", |_| Box::pin(async move {
             println!("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
@@ -79,6 +84,7 @@ async fn main() -> Result<(), rocket::Error> {
         })))
     }
 
+    // Set up template directory handling
     let conf: Result<Vec<String>, figment::Error> = rct.figment().extract_inner("template_dir");
     rct.manage(TemplateDir(if let Ok(dir) = conf {!dir.is_empty()} else {false}))
         .launch().await?;
